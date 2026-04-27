6c1af77327e22b271184a49aab52c00c89f007e9
Mickey777777 <wh879642@gmail.com>
2026-04-27 23:31:26 +0900

    main.py, rag_pipline 연결

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import rag_pipeline
 
 app = FastAPI()
 
@@ -6,3 +7,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/histolog/ai/query")
+async def query(query: str):
+    if(not query.strip()):
+        raise HTTPException(status_code=400, detail="empty query")
+    
+    answer = rag_pipeline.ask(query)
+
+    return {"query": query, "answer": answer}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
